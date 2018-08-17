79302ea028eea5894954513629d255fd3e60baf0
vagrant <vagrant@homestead>
2018-08-17 12:10:54 +0000

    Initial Commit

---
+++ app/Http/Controllers/PasswordResetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Password_Reset;
+use Illuminate\Http\Request;
+
+class PasswordResetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Password_Reset  $password_Reset
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Password_Reset $password_Reset)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Password_Reset  $password_Reset
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Password_Reset $password_Reset)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Password_Reset  $password_Reset
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Password_Reset $password_Reset)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Password_Reset  $password_Reset
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Password_Reset $password_Reset)
+    {
+        //
+    }
+}
---
+++ database/factories/PasswordResetFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Password_Reset::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
